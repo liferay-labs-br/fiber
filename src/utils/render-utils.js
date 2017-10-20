ec5ab21f717b483a41086cb9c7cc4f1a24306f82
Clóvis Neto <clovis.neto@liferay.com>
2017-10-20 18:56:15 -0300

    [FIX #9] Identify when the attribute is a function

--- src/utils/render-utils.js
+++ src/utils/render-utils.js
@@ -1,11 +1,37 @@
 export default class renderUtils {
-	static _formatAttributes(node, attributes) {
-		Object.keys(attributes).forEach( attr => {
-			let value = attributes[attr];
+	constructor(FiberComponent, node, attrs){
+		this.FiberComponent = FiberComponent;
+		this.node = node;
+		this.attrs = attrs;
+	}
+	
+	formatAttributes() {
+		let instance = this;
+		let attrs = instance.attrs;
+		
+		Object.keys(attrs).forEach(attr => {
 			
-			if(value === 'null' || value === 'undefined' || !value) return;
-
-			node.setAttribute(attr, value);
+			if(attrs[attr] === 'null' || attrs[attr] === 'undefined' || !attrs[attr]) return;
+			
+			if(instance._isEvent(attr)) return instance._addEventListener(attr);
+			
+			instance.node.setAttribute(attr, attrs[attr]);
 		} );
+	}
+
+	_addEventListener(attr){
+		let instance = this;
+		let eventName = instance._extractEventName(attr);
+		let FiberComponent = instance.FiberComponent;
+		
+		instance.node.addEventListener(eventName, instance.attrs[attr].bind(FiberComponent));
+	}
+
+	_isEvent(attr){
+		return /^on/.test(attr);
+	}
+
+	_extractEventName(attr){
+		return attr.slice('2').toLowerCase();
 	}
 }
