00fa7d3287e229615c01792646773bfe9ff87b3f
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-10-20 22:51:51 -0300

    Move render to folder renderers

--- src/renderers/render.js
+++ src/renderers/render.js
@@ -1,29 +1,31 @@
-import { buildComponentFromVNode } from './component.js';
-import RenderUtils from './utils/render-utils';
+import { buildComponentFromVNode } from '../isomorphic/createComponent';
+import RenderUtils from '../utils/render-utils';
 
 let FiberComponent;
 
-export default function render(vnode, parent) {
-	
+const render = (vnode, parent) => {
 	if (vnode == null || typeof vnode === 'boolean') vnode = '';
 
 	if (typeof vnode==='string') return document.createTextNode(vnode);
-	
+
 	if (typeof vnode.nodeName === 'function') {
 		FiberComponent = buildComponentFromVNode(vnode, {});
 		vnode = FiberComponent.vnode;
 	}
 
 	let node = document.createElement(vnode.nodeName);
-	
+
 	let attrs = vnode.attributes || {};
-	
+
 	let renderUtils = new RenderUtils(FiberComponent.instance, node, attrs);
 	renderUtils.formatAttributes();
 
 	(vnode.children || []).forEach( child => node.appendChild(render(child)) );
-	
+
 	if (parent) parent.appendChild(node);
-	
+
 	return node;
 }
+
+
+export default render;
