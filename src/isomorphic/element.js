e13e57750557263eff361f8127893139cf1535ac
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-10-21 16:00:48 -0300

    Converts the child with the type of number to string

--- src/isomorphic/element.js
+++ src/isomorphic/element.js
@@ -1,5 +1,11 @@
 const createElement = (nodeName, attributes, ...args) => {
-  let children = args.length ? [].concat(...args) : null;
+  let children, i;
+
+  for(i = 0; i < args.length; i++) {
+    if (typeof args[i] === 'number') args[i] = String(args[i]);
+  }
+
+  children = args.length ? [].concat(...args) : null;
 
   return { nodeName, attributes, children };
 }
