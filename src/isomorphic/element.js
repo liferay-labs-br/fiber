cc82a3a4f45ef9186b1bd878b715ba36cfbf8bb5
Matuzalém Teles <matuzalemteles@gmail.com>
2017-10-27 00:48:44 -0300

    Revert "Converts the child with the type of number to string"
    This reverts commit e13e57750557263eff361f8127893139cf1535ac.

--- src/isomorphic/element.js
+++ src/isomorphic/element.js
@@ -1,11 +1,5 @@
 const createElement = (nodeName, attributes, ...args) => {
-  let children, i;
-
-  for(i = 0; i < args.length; i++) {
-    if (typeof args[i] === 'number') args[i] = String(args[i]);
-  }
-
-  children = args.length ? [].concat(...args) : null;
+  let children = args.length ? [].concat(...args) : null;
 
   return { nodeName, attributes, children };
 }
