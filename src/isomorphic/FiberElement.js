3d20a87cc9dde9eaa4df9f98f2f45bea215291a6
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-11-14 01:23:10 -0300

    Add doc in FiberElement

--- src/isomorphic/FiberElement.js
+++ src/isomorphic/FiberElement.js
@@ -1,3 +1,10 @@
+/**
+ * Create element fiber with attributes and children's.
+ * @param nodeName name the tag
+ * @param attributes attributes that go through the tag
+ * @param args arguments with children's tags
+ * @return {object} return element a fiber.
+ */
 const createElement = (nodeName, attributes, ...args) => {
   let children = args.length ? [].concat(...args) : null;
 
